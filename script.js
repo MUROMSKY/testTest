3db3f76584c759b658fdf44657b0f6f099509fe9
ILYA MUROMSKY <proffas@bk.ru>
2023-02-11 22:22:59 +0300

    Added hw2

---
+++ script.js
@@ -0,0 +1,67 @@
+// 1 задача
+let a = 10;
+alert (a + 10);
+
+// 2 задача
+const release = '29 июня 2007 год';
+alert (release);
+
+
+// 3 задача
+const creator = 'Брендан Эйх';
+alert (creator);
+
+
+// 4 задача
+let number1 = 10;
+let number2 =2;
+alert(number1 + number2);
+alert(number1 - number2);
+alert(number1 * number2);
+alert(number1 / number2);
+
+
+// 5 задача
+let apple = 2;
+let result = 2**5;
+alert(result);
+
+
+// 6 задача
+let q = 9;
+let b = 2;
+alert(q % b);
+
+
+//7 задача
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /= 3;
+num += 1;
+num -= 1;
+alert(num);
+
+
+//8 задача
+let age1 = prompt("Сколько вам лет?");
+alert(age1);
+
+
+// 9 задача
+const user = {
+    Name: 'Jon',
+    age2: 23,
+    isAdmin: true,
+} 
+user["city of residence"] = 'Toronto';
+user.age2 = 30;
+delete user["city of residence"];
+let info = prompt("Какую информацию хотите узнать о пользователе?");
+alert(user[info]);
+
+
+// 10 задача
+let name1 = prompt("введите ваше имя");
+alert(`Привет, ${name1}!`);
